680a63ce67940c8f00627be7c6c7a01ecacb1e6d
alexR <alex.ramirez.valerin@gmail.com>
2022-07-03 17:50:36 -0600

    fix layout

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,14 +3,14 @@ export default function Header({ items = [] }) {
         <nav className='navbar px-14 bg-gradient-to-b from-gradientt to-gradientb shadow-md flex-initial'>
             <div className="flex-initial">
                 <a href='/' className="
-                    btn p-0 text-white btn-ghost hover:bg-transparent normal-case text-logo font-thin tracking-[13.5px] font-caslon uppercase">ADLYCEUM</a>
+                    p-0 text-white hover:bg-transparent normal-case text-logo font-thin tracking-[13.5px] font-caslon uppercase">ADLYCEUM</a>
             </div>
             <div className="flex-auto justify-end sm:justify-end gap-2 font-roboto py-1">
                 <ul className="menu menu-horizontal text-white p-0 mr-[-16px]">
                     {items.map((item, index) => (
                         <li key={`Header-nav-item-${index}`}>
                             <a
-                                className='hover:bg-transparent hover:underline hover:underline-offset-4 text-2xl'
+                                className='font-light decoration-1 hover:bg-transparent hover:underline hover:underline-offset-4 text-2xl'
                                 {...item.onClick ?
                                     { onClick: item.onClick } : { href: item.action }
                                 }>
