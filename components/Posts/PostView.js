fc263f0f192928238bc00ead4093d3ed2f372366
alexR <alex.ramirez.valerin@gmail.com>
2022-07-03 20:25:47 -0600

    fix layout

--- components/Posts/PostView.js
+++ components/Posts/PostView.js
@@ -11,28 +11,34 @@ const PostView = ({
     const isCurrentUserAuthor = author?.id === user?.id;
 
     const files = post?.attachments?.map(file =>
-        <a target='_blank' key={`file_attachment_${file.id}`} href={file.url} className='text-primary ml-4 text-xs underline underline-offset-1'>{file.title || file.filename}</a>
+        <a
+            target='_blank'
+            href={file.url}
+            download={file.title || file.filename}
+            key={`file_attachment_${file.id}`}
+            className='text-other hover:text-primary ml-4 underline underline-offset-1'>
+            {file.title || file.filename}
+        </a>
     );
 
     return (
         <article className='flex flex-col gap-4 p-2 items-stretch justify-start content-start flex-nowrap'>
-            <div className='flex flex-row items-center justify-between pb-2 border-2 border-transparent rounded-none border-b-black'>
+            <div className='flex flex-row items-center justify-between border-[1px] border-transparent rounded-none border-b-black'>
                 <h2 className="col-span-4 text-4xl">{post.title}</h2>
                 {isCurrentUserAuthor &&
-                    <a href={`/posts/${post.id}/edit`} className='align-self-end text-primary text-lg'>{'Editar Publicación >'}</a>
+                    <a href={`/posts/${post.id}/edit`} className='align-self-end text-primary text-2xl'>{'Editar Publicación >'}</a>
                 }
             </div>
             <div className='grid grid-cols-4 gap-4'>
                 <IFrame className='min-h-[70vh] col-span-3' srcDoc={post.monographView} />
-                <aside className='col-span-1 flex flex-col gap-2 pl-4 border-2 border-transparent rounded-none border-l-black'>
+                <aside className='col-span-1 flex flex-col gap-4 pl-4 border-2 border-transparent rounded-none border-l-black'>
                     {post.course &&
-                        <h3 className='text-lg'><span className='text-primary pr-2'>Curso:</span>{post.course.name}</h3>
+                        <h3 className='text-lg font-caslon'><span className='text-primary font-roboto text-xl pr-2'>Curso:</span>{post.course.name}</h3>
                     }
-                    <h4 className='text-xs'><span className='text-primary pr-2 text-lg'>Autor(es):</span>{author?.fullname || user?.fullname}</h4>
+                    <h4 className='text-lg font-caslon'><span className='text-primary font-roboto text-xl pr-2'>Autor(es):</span>{author?.fullname || user?.fullname}</h4>
                     {post?.coauthors?.length > 0 && Array.isArray(post.coauthors) ?
-                        post.coauthors.map(coauthor =>
-                            <h4 key={`co-autor_${coauthor.id}`} className='text-xs'>{coauthor.fullname}</h4>
-                        ) : <h4 className='text-xs'>{post?.coauthors.fullname}</h4>
+                        <h4 className='text-lg font-caslon'>{post.coauthors.map(coauthor => coauthor.fullname).join(', ')}</h4>
+                        : <h4 className='text-lg font-caslon'>{post?.coauthors.fullname}</h4>
                     }
                     <a onClick={toggleShowFiles} className='text-other hover:text-primary underline underline-offset-1'>Contenido Adjunto ></a>
                     {showFiles && files}
