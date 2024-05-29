7affe6c9a40974e1065061cb0951c0f50c003aad
superstar0215 <superstar00215@gmail.com>
2024-05-29 23:13:21 +0900

    add some comments and integrate Jodit editor

--- components/Editor/Editor.js
+++ components/Editor/Editor.js
@@ -1,16 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import dynamic from "next/dynamic";
-import "quill/dist/quill.snow.css";
+import Head from "next/head";
 
-const QuillEditorComponent = dynamic(
-  async () => {
-    const { default: ReactQuill } = await import("react-quill");
-    return ReactQuill;
-  },
-  {
-    ssr: false,
-  }
-);
+// Using dynamic import of Jodit component as it can't render in server side
+const JoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
 
 const Editor = ({
   editorContent,
@@ -19,66 +12,65 @@ const Editor = ({
   setSection,
   ...props
 }) => {
+  const editor = useRef(null);
+  // check if the content is loaded perfectly
   const [isBrowser, setIsBrowser] = useState(false);
+  // value of the Editor
+  const [model, setModel] = useState("");
+
   useEffect(() => {
     setIsBrowser(true);
   }, []);
 
-  const [model, setModel] = useState("");
-
   useEffect(() => {
     setModel(editorContent.innerHTML);
-    console.log(editorContent.innerHTML);
     setChangedContent(editorContent.innerHTML);
     try {
       const sectionTitleElement = editorContent.parentNode.getElementsByTagName("h2")[0];
-      if (sectionTitleElement?.id != 'title') {
+      if (sectionTitleElement?.id !== 'title') {
         setSection(sectionTitleElement.textContent);
       }
     } catch (e) {
-      console.log("Please select correct section");
+      console.log("Please select the correct section");
     }
   }, [editorContent]);
 
-  const handleModelChange = (content, delta, source, editor) => {
-    setChangedContent(content);
-    setModel(content);
+  const handleModelChange = (value) => {
+    console.log('this is value===>', value);
+    setChangedContent(value);
+    setModel(value);
   };
 
-  const modules = {
-    toolbar: [
-      [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      ['link', 'image'],
-      [{ 'align': [] }],
-      ['clean']
+  const config = useMemo(() => ({
+    readonly: false,
+    toolbar: true,
+    uploader: {
+      insertImageAsBase64URI: true,
+      imagesExtensions: ['jpg', 'png', 'jpeg', 'gif', 'svg', 'webp']
+    },
+    buttons: [
+      "undo", "redo", "bold", "italic", "link", "align", "image", "source", "fullsize"
     ],
-  };
-
-  const formats = [
-    'header', 'font',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet',
-    'link', 'image', 'align'
-  ];
+    placeholder: "Edite aquí su contenido!",
+  }), []);
 
   return (
     <div className="w-full h-full p-1">
+      <Head>
+        <title>Jodit Rich Text Editor</title>
+      </Head>
       <div className="flex justify-between">
         <span className="font-semibold text-[30px]">{section}</span>
       </div>
       <form className="w-full h-full mt-5">
         {isBrowser && (
-          <>
-            <QuillEditorComponent
-              value={model}
-              onChange={handleModelChange}
-              modules={modules}
-              formats={formats}
-              theme="snow"
-            />
-          </>
+          <JoditEditor
+            ref={editor}
+            value={model}
+            config={config}
+            onChange={handleModelChange}
+            className="w-full h-[70%] mt-10 bg-white"
+          />
         )}
       </form>
     </div>
