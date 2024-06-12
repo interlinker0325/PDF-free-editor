aa73fd08939e35866a791ab30f5dc060d91c90d3
superstar0215 <superstar00215@gmail.com>
2024-06-13 05:44:55 +0900

    complete Formulario compliance panel

--- components/Compliance/Compliance.js
+++ components/Compliance/Compliance.js
@@ -1,4 +1,11 @@
-const Compliance = () => {
+const Compliance = ({ form }) => {
+
+    const check = (
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 48 48">
+            <path fill="#2775db" d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"></path><path fill="#fff" d="M34.602,14.602L21,28.199l-5.602-5.598l-2.797,2.797L21,33.801l16.398-16.402L34.602,14.602z"></path>
+        </svg>
+    )
+
     return (
         <>
             <div className="flex justify-around mt-10 leading-8 text-xl">
@@ -25,22 +32,42 @@ const Compliance = () => {
                                     Términos de publicación
                                 </div>
                             </div>
-                            <div className="">
-                                <div className="text-red-600">
-                                    Pendiente
-                                </div>
-                                <div className="text-red-600">
-                                    Pendiente
-                                </div>
-                                <div className="text-red-600">
-                                    Pendiente
-                                </div>
-                                <div className="text-red-600">
-                                    Pendiente
-                                </div>
-                                <div className="text-red-600">
-                                    Pendiente
-                                </div>
+                            <div className="form-check">
+                                {form.course ?
+                                    check
+                                    :
+                                    <div className="text-red-600">
+                                        Pendiente
+                                    </div>
+                                }
+                                {form.coverimage ?
+                                    check
+                                    :
+                                    <div className="text-red-600">
+                                        Pendiente
+                                    </div>
+                                }
+                                {form.description ?
+                                    check
+                                    :
+                                    <div className="text-red-600">
+                                        Pendiente
+                                    </div>
+                                }
+                                {form.tags ?
+                                    check
+                                    :
+                                    <div className="text-red-600">
+                                        Pendiente
+                                    </div>
+                                }
+                                {form.agreedterms ?
+                                    check
+                                    :
+                                    <div className="text-red-600">
+                                        Pendiente
+                                    </div>
+                                }
                             </div>
                         </div>
                     </div>
