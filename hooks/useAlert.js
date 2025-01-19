181e8bf64955b0cc324e20072fe4e4b028c1d0c3
Andrés Suárez <andresfelipe9619@gmail.com>
2025-01-19 14:07:34 -0500

    Add useAlert hook and replace enqueueSnackbar calls
    Introduced a reusable `useAlert` hook to simplify notification logic. Replaced direct `enqueueSnackbar` calls in `usePost` with `useAlert` functions for better modularity and maintainability.

---
+++ hooks/useAlert.js
@@ -0,0 +1,32 @@
+import {enqueueSnackbar} from "notistack";
+
+const defaultOptions = {
+  preventDuplicate: true,
+  anchorOrigin: {
+    vertical: 'top',
+    horizontal: 'center'
+  }
+}
+
+export default function useAlert() {
+
+  function showSuccess(message, options = null) {
+    return enqueueSnackbar(message,
+        {
+          variant: 'success',
+          ...defaultOptions,
+          ...options
+        });
+  }
+
+  function showError(message, options = null) {
+    return enqueueSnackbar(message,
+        {
+          variant: 'error',
+          ...defaultOptions,
+          ...options
+        });
+  }
+
+  return {showSuccess, showError};
+}
