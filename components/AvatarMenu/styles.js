025f4a6661cc0629ae2366bd877de47577695df4
Andrés Suárez <andresfelipe9619@gmail.com>
2025-02-07 22:02:55 -0500

    🎨 Format code

--- components/AvatarMenu/styles.js
+++ components/AvatarMenu/styles.js
@@ -1,6 +1,6 @@
 export default {
     contAvatar: 'flex items-center overflow-hidden',
-    contBtn: 'text-white underline decoration-1 mx-[10px] border-bgButtonSecundary lg:px-[10px] text-2xl',
+    contBtn: 'text-white underline decoration-1 mx-[10px] border-bgButtonSecundary lg:px-[10px] text-2xl cursor-pointer',
     svgHamburger: 'w-[30px] h-[30px]',
     contBtnOptions: 'max-md:hidden cursor-pointer',
     contProfile: 'bg-transparent max-md:p-0 bg-transparent'
